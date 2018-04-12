23bca6f3f2c7717e70e0a33cb7ed742f64659abf
ths-laurent <thslaurent@gmail.com>
2018-04-12 15:45:19 +0200

    added getAllDivision method in repository + test

--- domain/src/test/java/be/dropdatabase/parkshark/domain/division/DivisionRepositoryTest.java
+++ domain/src/test/java/be/dropdatabase/parkshark/domain/division/DivisionRepositoryTest.java
@@ -1,13 +1,19 @@
 package be.dropdatabase.parkshark.domain.division;
 
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 @RunWith(SpringRunner.class)
@@ -17,15 +23,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DivisionRepositoryTest {
 
     @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
     private DivisionRepository divisionRepository;
+
+    @After
+    public void cleanUp() {
+      entityManager.getEntityManager().createQuery("delete from Division").executeUpdate();
+    }
 
 
     @Test
     public void saveDivision() {
         Division savedDivision = divisionRepository.createDivision(new Division());
         System.out.println(savedDivision.getDivisionId());
-        Assertions.assertThat(savedDivision.getDivisionId()).isNotNull();
+        assertThat(savedDivision.getDivisionId()).isNotNull();
     }
+
     @Test
     public void getByID() {
         Division savedDivision = divisionRepository.createDivision(new Division
@@ -35,8 +50,29 @@ public class DivisionRepositoryTest {
                 .withParentDivision(null).build());
         System.out.println(savedDivision.getDivisionId());
         Division searchecDivision = divisionRepository.findByName("Pomme");
-        Assertions.assertThat(savedDivision).isEqualToComparingFieldByFieldRecursively(searchecDivision);
+        assertThat(savedDivision).isEqualToComparingFieldByFieldRecursively(searchecDivision);
         System.out.println(searchecDivision.toString());
+    }
+
+    @Test
+    public void getAllDivisions() {
+        Division division1 = divisionRepository.createDivision(new Division
+                .DivisionBuilder().withDivisionName("dude")
+                .withOriginalnName("dudy")
+                .withDirector("dude1")
+                .withParentDivision(null).build());
+        Division division2 = divisionRepository.createDivision(new Division
+                        .DivisionBuilder().withDivisionName("Pomme")
+                        .withOriginalnName("Poire")
+                        .withDirector("John Malkovik")
+                        .withParentDivision(null).build());
+
+        assertThat(divisionRepository.getAllDivisions())
+                .size()
+                .isEqualTo(2)
+                .returnToIterable()
+                .contains(division1,division2);
+
     }
 
 
