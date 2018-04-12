d996e29817ce64cf0f3d46b84ae69443ae46f2aa
AlexSchaillee <alex.schaillee@cm.be>
2018-04-12 14:40:29 +0200

    creating MEMBER class + TABLE

--- domain/src/main/java/be/dropdatabase/parkshark/domain/member/Member.java
+++ domain/src/main/java/be/dropdatabase/parkshark/domain/member/Member.java
@@ -1,10 +1,28 @@
 package be.dropdatabase.parkshark.domain.member;
 
+import be.dropdatabase.parkshark.domain.licenseplate.LicensePlate;
 import be.dropdatabase.parkshark.domain.persondetails.PersonDetails;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MEMBERS")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBERS_SEQ")
+    @SequenceGenerator(name = "MEMBERS_SEQ", sequenceName = "MEMBERS_SEQ", allocationSize = 1, initialValue = 1)
+    @Column(name = "MEMBER_ID")
     private long memberId;
+    @OneToOne
+    @JoinColumn(name = "PERSON_DETAILS_ID")
     private PersonDetails personDetails;
+    @Embedded
+    private LicensePlate licensePlate;
+    @Column(name = "REGISTRATION_DATE")
+    private LocalDateTime registrationDate;
+
+
 
 }
