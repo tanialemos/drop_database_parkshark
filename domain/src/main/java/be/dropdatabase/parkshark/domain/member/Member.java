bc8db5fdeaff854428c29fd70b3d2424cf36ea2c
AlexSchaillee <alex.schaillee@cm.be>
2018-04-13 07:43:34 +0200

    creating MEMBER class + TABLE

--- domain/src/main/java/be/dropdatabase/parkshark/domain/member/Member.java
+++ domain/src/main/java/be/dropdatabase/parkshark/domain/member/Member.java
@@ -23,6 +23,65 @@ public class Member {
     @Column(name = "REGISTRATION_DATE")
     private LocalDateTime registrationDate;
 
+    public Member() {
+    }
+
+    private Member(long memberId, PersonDetails personDetails, LicensePlate licensePlate, LocalDateTime registrationDate) {
+        this.memberId = memberId;
+        this.personDetails = personDetails;
+        this.licensePlate = licensePlate;
+        this.registrationDate = registrationDate;
+    }
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public PersonDetails getPersonDetails() {
+        return personDetails;
+    }
+
+    public LicensePlate getLicensePlate() {
+        return licensePlate;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public static class MemberBuilder {
+        private long memberId;
+        private PersonDetails personDetails;
+        private LicensePlate licensePlate;
+        private LocalDateTime registrationDate;
+
+        public static MemberBuilder memberBuilder() {
+            return new MemberBuilder();
+        }
 
 
+        public MemberBuilder withMemberId(long memberId) {
+            this.memberId = memberId;
+            return this;
+        }
+
+        public MemberBuilder withPersonDetails(PersonDetails personDetails) {
+            this.personDetails = personDetails;
+            return this;
+        }
+
+        public MemberBuilder withLicensePlate(LicensePlate licensePlate) {
+            this.licensePlate = licensePlate;
+            return this;
+        }
+
+        public MemberBuilder withRegistrationDate(LocalDateTime registrationDate) {
+            this.registrationDate = registrationDate;
+            return this;
+        }
+
+        public Member createMember() {
+            return new Member(memberId, personDetails, licensePlate, registrationDate);
+        }
+    }
 }
